c914f50ea6ba2b6a1baab6b961fafac98cdc27d0
Adnan Ali <adnanalisukhera400@gmail.com>
2023-05-08 19:24:27 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  AppBar,
+  CssBaseline,
+  Button,
+  Toolbar,
+  Typography,
+  CardMedia,
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  CardActions,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+import useStyles from "./styles";
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="primary">
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              That's because Lorem ipsum is not a reserved keyword or function
+              in React or in Visual Studio Code. It is simply a string of
+              placeholder text that can be used in place of real content.
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container justifyContent="center" spacing={2}>
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See My Photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Call
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+
+        <Container maxWidth="md">
+          <Grid container spacing={4} className={classes.cardGrid}>
+            {arr.map((item) => {
+              return (
+                <Grid item key={item} xs={12} sm={6} lg={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      title="my title"
+                      image="https://source.unsplash.com/random"
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography variant="h5" gutterBottom>
+                        Heading
+                      </Typography>
+                      <Typography>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Fugit, adipisci!
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        New
+                      </Button>
+                      <Button size="small" color="primary">
+                        Edit
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
+      </main>
+    </>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,29 @@
+import { makeStyles } from "@material-ui/core";
+
+const usStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  icon: {
+    marginRight: "20px",
+  },
+  buttons: {
+    marginTop: "30px",
+  },
+  cardGrid: {
+    padding: "20px 0px",
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%",
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+export default usStyles;
